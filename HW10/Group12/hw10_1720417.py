01df55da1116e579cc6d3f66b7edeef9241b12a7
newplayer233 <1794528220@qq.com>
2020-05-20 23:51:01 +0800

    hw10_1720417

--- HW10/Group12/hw10_1720417.py
+++ HW10/Group12/hw10_1720417.py
@@ -12,7 +12,7 @@ import sqlite3
 conn = sqlite3.connect('C:\\git\\ljy.db') 
 print("Opened database successfully");
 
-conn.execute('''CREATE TABLE addressbook 
+conn.execute('''CREATE TABLE USER 
 (ID INT PRIMARY KEY     NOT NULL, 
 NAME           TEXT    NOT NULL,  
 TELEPHONE      CHAR(50) NOT NULL,
@@ -20,19 +20,19 @@ COMPANY        TEXT    NOT NULL,
 ADDRESS        TEXT    NOT NULL);''') 
 print("Table USER created successfully");
 
-conn.execute("INSERT INTO addressbook (ID,NAME,TELEPHONE,COMPANY,ADDRESS) \
+conn.execute("INSERT INTO USER (ID,NAME,TELEPHONE,COMPANY,ADDRESS) \
     VALUES (1, 'zxx', 15554781548, 'a公司','浦东')")
-conn.execute("INSERT INTO addressbook (ID,NAME,TELEPHONE,COMPANY,ADDRESS) \
-    VALUES (2, 'wxx', 18129328324, 'b公司','浦东')") 
+conn.execute("INSERT INTO USER (ID,NAME,TELEPHONE,COMPANY,ADDRESS) \
+    VALUES (2, 'wxx', 18129328324, 'b公司','普陀')") 
 
 conn.commit() 
 num1=conn.total_changes 
-print("{0} rows changed in table addressbook.".format(num1))   
+print("{0} rows changed in table USER.".format(num1))   
 
 conn = sqlite3.connect('C:\\git\\ljy.db') 
 print("Opened database successfully") 
 
-cursor1 = conn.execute("SELECT ID,NAME,TELEPHONE,COMPANY,ADDRESS from addressbook where NAME='ljy'") 
+cursor1 = conn.execute("SELECT ID,NAME,TELEPHONE,COMPANY,ADDRESS from USER where NAME='ljy'") 
 for row in cursor1: 
     print("ID = ", row[0]) 
     print("NAME = ", row[1]) 
@@ -40,7 +40,7 @@ for row in cursor1:
     print("COMPANY = ", row[3])
     print("ADDRESS = ", row[4])
 
-conn.execute("delete from addressbook where NAME='cyc'") 
+conn.execute("delete from USER where NAME='cyc'") 
 conn.commit() 
 print("Total number of rows updated :", conn.total_changes) 
 num1=conn.total_changes 
