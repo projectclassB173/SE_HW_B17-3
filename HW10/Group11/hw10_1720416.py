ada51a48ddefebb95c100f419028816a1e3530d7
xxy624 <12360906@qq.com>
2020-05-21 12:44:34 +0800

    .

--- HW10/Group11/hw10_1720416.py
+++ HW10/Group11/hw10_1720416.py
@@ -1,61 +1,99 @@
-# 编写一个Python程序，采用SQLite数据库实现通讯录管理功能。 
-# 采用SQLite数据库存放个人通讯录，要求存放联系人的姓名、电话、公司、地址； 
-# 设计相应的函数完成以下数据库操作： 
-# 创建数据库表； 
-# 新增联系人； 
-# 按姓名查询联系人详细信息； 
-# 删除联系人
+#编写一个Python程序，采用SQLite数据库实现通讯录管理功能。
+#采用SQLite数据库存放个人通讯录，要求存放联系人的姓名、电话、公司、地址；
+#设计相应的函数完成以下数据库操作：
+#创建数据库表；
+#新增联系人；
+#按姓名查询联系人详细信息；
+#删除联系人；
 
 import sqlite3
-conn = sqlite3.connect('D:\\课程\\大三下\\test.db')
-print("success");
 
-conn.execute('''CREATE TABLE USER
-(ID INT PRIMARY KEY     NOT NULL, 
-NAME           TEXT    NOT NULL,  
-TELEPHONE      CHAR(25) NOT NULL,
-COMPANY        CHAR(50)    NOT NULL,
-ADDRESS        TEXT    NOT NULL);''')
-print("success");
+conn=sqlite3.connect('D:\\课程\\大三下\\项目实战\\本地数据库\\practice.db')
+print("成功打开practice数据库")
 
-conn.execute("INSERT INTO USER (ID,NAME,TELEPHONE,COMPANY,ADDRESS) \
-    VALUES (1, '1', 12121212121, '1公司','上海')")
-conn.execute("INSERT INTO USER (ID,NAME,TELEPHONE,COMPANY,ADDRESS) \
-    VALUES (2, '2', 12345678901, '2公司','北京')")
-conn.execute("INSERT INTO USER (ID,NAME,TELEPHONE,COMPANY,ADDRESS) \
-    VALUES (3, '3', 11111111111, '3公司','广州')")
-conn.commit()
-num1=conn.total_changes
-print("{0} rows changed in table USER.".format(num1))
+conn.execute('CREATE TABLE AddressBook(NAME PRIMARY KEY NOT NULL,TELEPHONE CHAR NOT NULL,COMPANY CHAR NOT NULL,ADDRESS CHAR NOT NULL);')
+print("成功创建AddressBook表")
 
-conn = sqlite3.connect('D:\\课程\\大三下\\test.db')
-print("Opened database successfully")
-cursor1 = conn.execute("SELECT ID,NAME,TELEPHONE,COMPANY,ADDRESS from USER where ID=2")
-for row in cursor1:
-   print("ID = ", row[0])
-   print("NAME = ", row[1])
-   print("TELEPHONE = ", row[2])
-   print("COMPANY = ", row[3])
-   print("ADDRESS = ", row[4])
-
-conn = sqlite3.connect('D:\\课程\\大三下\\test.db')
-print("Opened database successfully")
-conn.execute("delete from USER where COMPANY='2公司'")
-conn.commit()
-print("Total number of rows updated :", conn.total_changes)
-num1=conn.total_changes
-print("{0} rows changed in table USER.".format(num1))
-
-conn = sqlite3.connect('D:\\课程\\大三下\\test.db')
-print("Opened database successfully")
-conn.execute("UPDATE USER set Company = '1公司' where ID=1")
-conn.commit()
-print("Total number of rows updated :", conn.total_changes)
-cursor1 = conn.execute("SELECT ID,NAME,TELEPHONE,COMPANY,ADDRESS from USER where ID=1")
-for row in cursor1:
-   print("ID = ", row[0])
-   print("NAME = ", row[1])
-   print("TELEPHONE = ", row[2])
-   print("COMPANY = ", row[3])
-   print("ADDRESS = ", row[4])
+m = int(input("请选择需要使用的功能：\n1.新增联系人\n2.按姓名查询联系人详细信息\n3.删除联系人\n"))
+if m==1:
+    conn = sqlite3.connect('D:\\课程\\大三下\\项目实战\\本地数据库\\practice.db')
+    name = input("请输入联系人姓名：\n")
+    telephone = input("请输入联系人电话：\n")
+    company = input("请输入联系人公司：\n")
+    address = input("请输入联系人地址：\n")
+    conn.execute("INSERT INTO AddressBook (NAME,TELEPHONE,COMPANY,ADDRESS) VALUES ('"+ name +"','"+ telephone +"','"+ company +"','"+ address +"')")
+    print("成功新增联系人{0}".format(name))
+    conn.commit()
+elif m==2:
+    conn = sqlite3.connect('D:\\课程\\大三下\\项目实战\\本地数据库\\practice.db')
+    name = input("请输入要查询的联系人姓名：\n")
+    search = conn.execute("SELECT NAME,TELEPHONE,COMPANY,ADDRESS FROM AddressBook WHERE NAME='"+ name +"'")
+    for row in search:
+        print("联系人姓名：",row[0])
+        print("联系人电话：",row[1])
+        print("联系人公司：",row[2])
+        print("联系人地址：",row[3])
+    conn.commit()
+elif m==3:
+    conn = sqlite3.connect('D:\\课程\\大三下\\项目实战\\本地数据库\\practice.db')
+    name = input("请输入要删除的联系人姓名：\n")
+    conn.execute("DELETE FROM AddressBook WHERE NAME='"+ name +"'")
+    print("成功删除联系人{0}".format(name))
+    conn.commit()
+else:
+    print("输入的内容有误，请输入123中的任意数字")
 conn.close()
+
+
+>>>
+============== RESTART: D:\课程\大三下\项目实战\课后作业\Python\hw10_1720416.py =============
+成功打开practice数据库
+请选择需要使用的功能：
+1.新增联系人
+2.按姓名查询联系人详细信息
+3.删除联系人
+1
+请输入联系人姓名：
+xxy
+请输入联系人电话：
+1720416
+请输入联系人公司：
+shanghai
+请输入联系人地址：
+shanghai
+成功新增联系人xxy
+>>>
+============== RESTART: D:\课程\大三下\项目实战\课后作业\Python\hw10_1720416.py =============
+成功打开practice数据库
+请选择需要使用的功能：
+1.新增联系人
+2.按姓名查询联系人详细信息
+3.删除联系人
+2
+请输入要查询的联系人姓名：
+xxy
+联系人姓名： xxy
+联系人电话： 1720416
+联系人公司： shanghai
+联系人地址： shanghai
+>>>
+============== RESTART: D:\课程\大三下\项目实战\课后作业\Python\hw10_1720416.py =============
+成功打开practice数据库
+请选择需要使用的功能：
+1.新增联系人
+2.按姓名查询联系人详细信息
+3.删除联系人
+3
+请输入要删除的联系人姓名：
+xxy
+成功删除联系人xxy
+>>>
+============== RESTART: D:\课程\大三下\项目实战\课后作业\Python\hw10_1720416.py =============
+成功打开practice数据库
+请选择需要使用的功能：
+1.新增联系人
+2.按姓名查询联系人详细信息
+3.删除联系人
+4
+输入的内容有误，请输入123中的任意数字
+>>>
